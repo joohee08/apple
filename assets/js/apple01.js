fa3d4f5ddce0445b923c9853778324d904beaf88
joohee08 <woohyun7722@naver.com>
2025-02-19 16:15:13 +0900

    g

--- assets/js/apple01.js
+++ assets/js/apple01.js
@@ -14,7 +14,7 @@ function initSwipers() {
         freeMode: true,
         loop: false,
         breakpoints: {
-            320: { slidesPerView: 2 }, // 아주 작은 모바일
+            320: { slidesPerView: 3 }, // 아주 작은 모바일
             768: { slidesPerView: 3 },   // 태블릿 이상
             1024: { slidesPerView: 3 }   // PC 이상
           }
